212d2faa7baec8c96f51c9830ac41c3a66ce8cab
Mario Lopez <mario@techmunchies.net>
2016-10-25 09:57:25 -0400

    Modified to support dependency install failure, will clean up code to detect the failure first, then install deps manually

--- boxstarter.ps1
+++ boxstarter.ps1
@@ -2,7 +2,8 @@ $WinlogonPath = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon"
 Remove-ItemProperty -Path $WinlogonPath -Name AutoAdminLogon
 Remove-ItemProperty -Path $WinlogonPath -Name DefaultUserName
 
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mwrock/boxstarter/master/BuildScripts/bootstrapper.ps1'))
+#iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mwrock/boxstarter/master/BuildScripts/bootstrapper.ps1'))
+. a:\Install-Boxstarter.ps1
 Get-Boxstarter -Force
 
 $secpasswd = ConvertTo-SecureString "vagrant" -AsPlainText -Force
